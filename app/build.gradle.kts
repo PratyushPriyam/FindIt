99c5d684b248099a2b755a9c3ce8852418e99187
praty <pratyushpriyam49@gmail.com>
2024-05-19 01:53:28 +0530

    Final Changes Done.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("com.github.bumptech.glide:glide:4.12.0") // Glide dependency
+    implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("com.airbnb.android:lottie:5.2.0")
 }
